d7cbfa2e80339e29e3d3ae1850072ef53a056017
Marcelo Parabocz <macerno@gmail.com>
2026-03-02 16:13:23 -0300

    project created

---
+++ src/main/java/com/aracnideo/sbhibernate/SbhibernateApplication.java
@@ -0,0 +1,13 @@
+package com.aracnideo.sbhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbhibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbhibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aracnideo/sbhibernate/SbhibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aracnideo.sbhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbhibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
