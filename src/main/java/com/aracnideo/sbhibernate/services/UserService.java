a611f0b13ad26649dd1205a26c21e05d49ae2955
Marcelo Parabocz <macerno@gmail.com>
2026-03-05 21:27:18 -0300

    feat: exception handling on user findbyid

--- src/main/java/com/aracnideo/sbhibernate/services/UserService.java
+++ src/main/java/com/aracnideo/sbhibernate/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.aracnideo.sbhibernate.entities.User;
 import com.aracnideo.sbhibernate.repositories.UserRepository;
+import com.aracnideo.sbhibernate.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
